cdcd97d243346ea72a4205ba4fe3740f9a8d5be0
tolcey <tolgaceyhan89@gmail.com>
2024-07-06 16:19:49 +0300

    Add files via upload

---
+++ Kilitli Kullanıcı Listesi Mail Atmak.ps1
@@ -0,0 +1,20 @@
+﻿$tarih= (get-date).ToString("dd-MM-yy")
+
+Search-ADAccount -LockedOut -UsersOnly -ResultPageSize 2000 -resultSetSize $null | Select-Object Name, SamAccountName, DistinguishedName | Export-CSV “C:\KilitliKullanıcılar-$tarih.csv” -NoTypeInformation
+
+$MailSender = " Gönderenin Adı<E-posta Adresi>" #Örnek; Tolga Ceyhan <iletisim@tolgaceyhan.com#
+$Subject = "E-Posta Başlığı" #Epostanıza başlık giriniz.#
+$EmailBody = "Kilitli kullanıcı listesine ekte ulaşabilirsiniz"
+$SMTPServer = 'SMTP Adresi' #SMTP adresini giriniz.#
+$filepath = "C:\KilitliKullanıcılar-$tarih.csv" 
+
+If ((Get-Item $filepath).Length -gt 0kb) { 
+Send-MailMessage -Encoding UTF8 -To "Alıcının E-posta Adresi" `
+-From $MailSender -SmtpServer $SMTPServer `
+-Subject $Subject `
+-BodyAsHtml $EmailBody `
+-Attachments $filepath `
+
+}
+else
+{}
